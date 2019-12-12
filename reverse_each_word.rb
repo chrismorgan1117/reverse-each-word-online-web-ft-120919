9227fed97a8c365a9ac375d23989f069e4958e42
Christopher Morgan <cwm111794@gmail.com>
2019-12-12 01:37:26 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 def reverse_each_word(sentance1)
   reversed = []
   array = [sentance1.split]
-  array.each{|word| reversed << array.reverse(word)}
+  array.each{|word| reversed.push(array.reverse(word))}
   return reversed
 end 
